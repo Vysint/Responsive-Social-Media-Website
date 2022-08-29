60ee920cb3b8f8ba5c21f1b6d1c0d6d64355d35b
Vincent Kipngetich <vinniebluz@icloud.com>
2022-08-29 23:58:33 +0300

    Added keyup event handler to the search bar to search through the messages

--- JS/main.js
+++ JS/main.js
@@ -4,6 +4,8 @@ const menuItems = document.querySelectorAll('.menu-item');
 // MESSAGES
 const messagesNotification = document.querySelector('#messages-notification');
 const messages = document.querySelector('.messages');
+const message = messages.querySelectorAll('.message');
+const messageSearch = document.querySelector('#message-search');
 
 
 // remove active class from all menu items
@@ -29,8 +31,21 @@ menuItems.forEach(item => {
         }
     })
 })
-
-// MESSAGES
+//MESSAGES
+//Search chat
+const searchMessage = () => {
+    const val = messageSearch.value.toLowerCase();
+    message.forEach(user => {
+        let name = user.querySelector('h5').textContent.toLowerCase();
+        if (name.indexOf(val) != -1){
+            user.style.display = 'flex';
+        }else{
+            user.style.display = "none";
+        }
+    })
+}
+messageSearch.addEventListener('keyup', searchMessage);
+// highlight messages card when messages menu is clicked.
 messagesNotification.addEventListener('click', () => {
     messages.style.boxShadow = '0 0 1rem var(--color-primary)';
     messagesNotification.querySelector('.notification-count').style.display = "none";
@@ -38,3 +53,4 @@ messagesNotification.addEventListener('click', () => {
         messages.style.boxShadow = "none";
     }, 2000)
 })
+
